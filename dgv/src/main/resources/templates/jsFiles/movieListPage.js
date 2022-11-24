a05fd33e1abd9b4e5e4fd55a6c1608d60dc0b7f5
Jang Jidam <jidam03@dongguk.edu>
2022-11-24 15:00:03 +0900

    login, signup

---
+++ dgv/src/main/resources/templates/jsFiles/movieListPage.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    console.log(`js file loaded`);
+
+    //import header, footer, navbar
+    $(`#header`).load(`../templates/layouts/header.html`);
+    $(`#footer`).load(`../templates/layouts/footer.html`);
+    $(`#navbar`).load(`../templates/layouts/navbar.html`);
+});
