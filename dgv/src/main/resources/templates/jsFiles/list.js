8757c6d786f5c9c0146ee4b303233a8c7b995ea9
apple4ree <cdi6578296@gmail.com>
2022-11-26 03:38:11 +0900

    2022/11/26

---
+++ dgv/src/main/resources/templates/jsFiles/list.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    console.log(`js file loaded`);
+
+    //import header, footer, navbar
+    $(`#header`).load(`../templates/layouts/header.html`);
+    $(`#footer`).load(`../templates/layouts/footer.html`);
+    $(`#navbar`).load(`../templates/layouts/navbar.html`);
+
+    $(`.button1`).click(function(){
+
+        location.href = 'payback.html';
+
+    });
+    $(`.button2`).click(function(){
+
+        localStorage.setItem(`movieName`,$(this).attr(`movieName`));
+        location.href = 'seats.html';
+
+        });
+
+
+});
